39703d6772618df690e7777cdb4e8df1bcad125e
yavetisyan <mr.avetisyan@yahoo.com>
2021-09-01 22:28:21 +0400

    asd

--- js/html_script.js
+++ js/html_script.js
@@ -1,28 +1,50 @@
-let div =document.getElementsByTagName('div')
+let div =document.getElementsByTagName('div');
 console.log(div);
 
-let name= document.getElementsByName('email')
-console.log(name);
+//let name= document.getElementsByName('email')
+//console.log(name);
 
-let id = document.getElementById('div5')
+//let id = document.getElementById('div5')
 
-console.log(id);
+//console.log(id);
  
-let clas= document.getElementsByClassName('asd')
-console.log(clas);
+//let clas= document.getElementsByClassName('asd')
+//console.log(clas);
 
-let query = document.querySelector('div')
-console.log(query);
+//let query = document.querySelector('div')
+//console.log(query);
 
-let queryAll = document.querySelectorAll('div')
-console.log(queryAll);
+//let queryAll = document.querySelectorAll('div')
+//console.log(queryAll);
 
-let queryClass = document.querySelectorAll('.asd')
-console.log(queryClass);
+//let queryClass = document.querySelectorAll('.asd')
+//console.log(queryClass);
 
-let attr = document.querySelectorAll('[attr1]')
-console.log(attr);
-
-
+//let attr = document.querySelectorAll('[attr1]')
+//console.log(attr);
 
 
+//let name1 = document.getElementsByName('checkbox');
+//console.log(name1);
+
+let id = document.getElementById('div5');
+console.log(id);
+
+id.style.backgroundColor = 'red';
+
+let a = document.querySelector('div');
+
+console.log(a);
+
+
+
+
+a.onclick(function(){
+
+
+	if(this.style.backgroundColor === 'none'){
+		this.style.backgroundColor = 'aqua';
+	} 
+	
+
+})
